5b8d3f1c0506748d0df030c5a4e7a4901383b0de
dsmnv <dmitry.d.sem@gmail.com>
2022-09-18 13:00:39 +0300

    variables.py added

---
+++ tests/variables.py
@@ -0,0 +1,7 @@
+class student:
+    first_name = 'Dmitry'
+    last_name = 'Sem'
+    email = 'demotest@tete.com'
+    mobile_number = '9570000000'
+    current_address = 'Saint - P'
+    permanent_address = 'Saint - P / permanent'
